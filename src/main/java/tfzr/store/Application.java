b6d61901d84b3be20b30208d1b0c98b81ec23731
Hani Al-Hamidawi <hani.al-hamidawi@consulteer.com>
2021-07-06 21:54:30 +0200

    neki msg

--- src/main/java/tfzr/store/Application.java
+++ src/main/java/tfzr/store/Application.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+	/**
+	 * lol
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
