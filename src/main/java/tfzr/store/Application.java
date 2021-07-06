78e2052b7f7e236a34c9b703c82df1beb46e1dab
Hani Al-Hamidawi <hani.al-hamidawi@consulteer.com>
2021-07-06 22:26:22 +0200

    neki msg jbt

--- src/main/java/tfzr/store/Application.java
+++ src/main/java/tfzr/store/Application.java
@@ -11,6 +11,6 @@ public class Application {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		//komentar neki
+		//komentar neki u picku materinu u tri lepejajaja
 	}
 }
